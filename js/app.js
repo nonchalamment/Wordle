e4dfe4ec003ff1177a851fef51b160ed60d42b59
nonchalamment <nonchalamment@gmail.com>
2023-04-04 18:23:26 -0400

    complete data file

--- js/app.js
+++ js/app.js
@@ -135,14 +135,7 @@ function updateRowColor() {
     }
     // if all letters are correct, there is no need for additional calculation for color.
     else {
-        let letterTally = word.reduce(function(prev, letter){
-            if(prev[letter]){
-              prev[letter] = prev[letter] + 1
-            } else {
-              prev[letter] = 1
-            }
-            return prev
-            }, {})
+        let letterTally = reduceArrayIntoTally(word)
         // otherwise, we create a tally of all the letters in the solution to deal with duplicate letters
         // in order to correctly render the greens and the yellows.
 
@@ -154,15 +147,26 @@ function updateRowColor() {
     }
 }
 
+function reduceArrayIntoTally(arr) {
+    return arr.reduce(function(prev, letter){
+        if(prev[letter]){
+          prev[letter] = prev[letter] + 1
+        } else {
+          prev[letter] = 1
+        }
+        return prev
+        }, {})
+}
+
 function renderIfGreen(largerIdx, smallerIdx, tally) {
     for (let i = largerIdx; i < largerIdx + 5; i++) {
-        let currentLetter = userInput[smallerIdx]
+        let currentLetter = `${userInput[smallerIdx]}`
         // iterating through 0-4 for each 5-letter input
         if (currentLetter === word[smallerIdx]) {
             squareEls[i].style.backgroundColor = "#7ba768"
             squareEls[i].style.border = "solid #7ba768"
             squareEls[i].style.color = "white"
-            tally[`${currentLetter}`] = tally[`${currentLetter}`] - 1
+            tally[currentLetter] = tally[currentLetter] - 1
         // if input at an index is the same is the word at the same index, then change background color
         // to green and decrease the tally count of the letter
         }
@@ -173,18 +177,18 @@ function renderIfGreen(largerIdx, smallerIdx, tally) {
 
 function renderIfYellow(largerIdx, smallerIdx, tally) {
     for (let i = largerIdx; i < largerIdx + 5; i++) {
-        let currentLetter = userInput[smallerIdx]
-        if (word.includes(`${currentLetter}`) && tally[currentLetter] > 0 && currentLetter != word[smallerIdx]) {
+        let currentLetter = `${userInput[smallerIdx]}`
+        if (word.includes(currentLetter) && tally[currentLetter] > 0 && currentLetter != word[smallerIdx]) {
             // if the letter is included in the word, but also isn't the right letter, but also
             // if the tally is remaining for that letter
             squareEls[i].style.backgroundColor = "#c4b560"
             squareEls[i].style.border = "solid #c4b560"
             squareEls[i].style.color = "white"
-            tally[`${currentLetter}`] = tally[`${currentLetter}`] - 1
+            tally[currentLetter] = tally[currentLetter] - 1
         }
         else if (currentLetter != word[smallerIdx]) {
-            squareEls[i].style.backgroundColor = "#5c5c5c"
-            squareEls[i].style.border = "solid #5c5c5c"
+            squareEls[i].style.backgroundColor = "#797c7e"
+            squareEls[i].style.border = "solid #797c7e"
             squareEls[i].style.color = "white"
         }
         smallerIdx++
